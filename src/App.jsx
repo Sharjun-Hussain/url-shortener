69739200d4b910c419bedd933f97a3105c5ea03e
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-03-28 17:29:33 +0530

    UI Designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Col, Container, Row } from "react-bootstrap";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, seturl] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-center">URL Shortener</h1>
+            <h5 className="text-center" style={{ opacity: 0.7 }}>
+              You can generate the shorten url by pasting ur long URLs
+            </h5>
+          </Col>
+
+          <div className="d-flex justify-content-center">
+            <input
+              className="urlinput"
+              type="text"
+              name="url"
+              placeholder="Enter URL"
+              value={url}
+              onChange={(e) => seturl(e.target.value)}
+            />
+            <Button className="btn">Shorten URL</Button>
+          </div>
+
+          <h5 className="text-center mt-5" style={{ opacity: 0.7 }}>
+           Your Shourten URl is : 
+          </h5>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
